9962427871d8e001c0fa3aa5b0e9b431e2b1c026
nikhilmukati <nmukati@qodeleaf.com>
2022-03-28 12:24:56 +0530

    changes made after review

--- displayUser.js
+++ displayUser.js
@@ -1,23 +1,14 @@
-function showData() {
+function displayUsers() {
   let userRecords = JSON.parse(localStorage.getItem("users")) || [];
   var tableElement = document.getElementById("data-table");
   userRecords.forEach((element) => {
     let row = document.createElement("tr");
     row.className = "row-data";
-    let rowData = document.createElement("td");
-    rowData.innerHTML = element["user-name"];     
-    console.log(element["user-name"]);
-    row.appendChild(rowData);
-    rowData = document.createElement("td");
-    rowData.innerHTML = element["first-name"];
-    row.appendChild(rowData);
-    rowData = document.createElement("td");
-    rowData.innerHTML = element["last-name"];
-    row.appendChild(rowData);
-    rowData = document.createElement("td");
-    rowData.innerHTML = element["email-address"];
-    row.appendChild(rowData);
-    tableElement.appendChild(row);
-  });
+    ["user-name","first-name","last-name","email-address"].map(item=>{
+      let rowData = document.createElement("td");
+      rowData.innerHTML = element[item];
+      row.appendChild(rowData);
+      tableElement.appendChild(row);})
+      });
 }
-showData();
+displayUsers();
--- signup.js
+++ signup.js
@@ -6,20 +6,17 @@ function saveData() {
   let firstName = document.getElementById("first-name").value;
   let lastName = document.getElementById("last-name").value;
   let password = document.getElementById("set-password").value;
-
   let userRecords = JSON.parse(localStorage.getItem("users")) || [];
   if (userRecords.find((v) => v["email-address"] === email)) {
     alert("User already exists with this email Kindly Login");
   } else {
-    console.log("userRecords-1", userRecords);
     userRecords.push({
       "user-name": userName,
       "email-address": email,
       "first-name": firstName,
       "last-name": lastName,
-       "password":password,
+      password: password,
     });
-    console.log("userRecords-1", userRecords);
     localStorage.setItem("users", JSON.stringify(userRecords));
   }
 }
